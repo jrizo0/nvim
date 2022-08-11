0167c1895b204c27f86453a74f25048597c6b8f4
jrizo0 <jr.rizo.o.jr@gmail.com>
2022-08-10 21:02:42 -0500

    term size

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -178,7 +178,7 @@ local mappings = {
     t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
     p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
     f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
-    h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
+    h = { "<cmd>ToggleTerm size=18 direction=horizontal<cr>", "Horizontal" },
     v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
   },
 }
